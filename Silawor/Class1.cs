bd391f6cbbed70a37f2a2d530faf0709b157f44c
Polkovnik-Daniil <0205danik@mail.ru>
2020-10-12 11:39:43 +0300

    Begin

--- Silawor/Class1.cs
+++ Silawor/Class1.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Silawor
 {
+    enum enum1
+    {
+        value = 1,
+        value_ = 2
+    }
+    struct struct_ {
+        public string name;
+        public int age;
+        public void GetInfo( )
+        {
+            Console.WriteLine($"Value name is : {name} ,value age is {age}");
+        }
+    }
     class Class1
     {
+        static void Main(string[] args)
+        {
+            enum1 en;
+            en = enum1.value;
+            Console.WriteLine($"Value enum is : {(int)en}");
+            struct_ st;
+            st.age = 20;
+            st.name = "Daniil";
+            Console.WriteLine($"Value struct is : {st.GetInfo()}");
+            Console.ReadKey();
+        }
     }
 }
